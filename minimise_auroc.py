1afb33577c861d9c278e42f5b53ec68626150fca
JoshuaHarper98 <62307943+JoshuaHarper98@users.noreply.github.com>
2023-05-15 20:09:54 +0100

    Create minimise_auroc.py

---
+++ minimise_auroc.py
@@ -0,0 +1,5 @@
+def minimise_auroc(y_true, y_pred):
+    metric_name = "minimise_auroc"
+    value = 1 - roc_auc_score(y_true, y_pred)
+    is_higher_better = False
+    return metric_name, value, is_higher_better
